24c2f90b826af2985f9d290cd32894253f865345
piotr-woz <piotrwozniak.dev@gmail.com>
2025-02-15 23:48:21 +0100

    Add unit tests to AppComponent

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,67 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+  });
+
+  afterEach(() => {
+    sessionStorage.clear();
+  });
+
+  it('should create the app', () => {
+    expect(app).toBeTruthy();
+  });
+
+  it('should react to isIntro signal change', () => {
+    app.isIntro.set(false);
+    fixture.detectChanges();
+    expect(app.isIntro()).toBeFalse();
+    app.isIntro.set(true);
+    fixture.detectChanges();
+    expect(app.isIntro()).toBeTrue();
+  });
+
+  it(`should keep isIntro as false when savedIntroState has value 'false'`, () => {
+    sessionStorage.setItem('introSignalState', 'false');
+    app.ngOnInit();
+    expect(app.isIntro()).toBeFalse();
+  });
+
+  it('should keep isIntro as true when savedIntroState is null', () => {
+    sessionStorage.removeItem('introSignalState');
+    app.ngOnInit();
+    expect(app.isIntro()).toBeTrue();
+  });
+
+  it('should call onCloseIntro when <app-intro> emits (closed) if isIntro is true', () => {
+    app.isIntro.set(true);
+    fixture.detectChanges();
+    spyOn(app, 'onCloseIntro');
+    const introComponent =
+      fixture.debugElement.nativeElement.querySelector('app-intro');
+    const event = new Event('closed');
+    introComponent.dispatchEvent(event);
+    expect(app.onCloseIntro).toHaveBeenCalled();
+  });
+
+  it('should set isIntro to false and store it in sessionStorage when onCloseIntro is called', () => {
+    app.onCloseIntro();
+    expect(app.isIntro()).toBeFalse();
+    expect(sessionStorage.getItem('introSignalState')).toEqual('false');
+  });
+
+  it('should keep isIntro as true when savedIntroState is invalid JSON', () => {
+    sessionStorage.setItem('introSignalState', 'invalid_json');
+    expect(() => app.ngOnInit()).toThrowError(SyntaxError);
+  });
+});
